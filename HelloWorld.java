6cfbade97b648d7a896d30a41602c45490e0f3f2
zlataaaaaa <71872348+zlataaaaaa@users.noreply.github.com>
2020-10-01 23:22:10 +0300

    Add files via upload
    сортировка столбцов

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,35 +3,34 @@ import java.util.Scanner;
 public class Day {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int r = in.nextInt();
         int n = in.nextInt();
-        int k = in.nextInt();
-        int[][] a = new int[n][k];
-        int[][] b = new int[n][k];
-        int[][] c = new int[n][k];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < k; j++) {
+        int[][] a = new int[r][n];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < n; j++) {
                 a[i][j] = in.nextInt();
             }
         }
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < k; j++) {
-                b[i][j] = in.nextInt();
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < k; j++) {
-                c[i][j] = 0;
-                for (int p = 0; p < k; p++) {
-                    c[i][j] += a[i][p] * b[p][j];
+            for (int j = 0; j < n - i; j++) {
+                if (j != n - i - 1) {
+                    if (a[0][j] > a[0][j + 1]) {
+                        for (int k = 0; k < r; k++) {
+                            int temp = a[k][j];
+                            a[k][j] = a[k][j + 1];
+                            a[k][j + 1] = temp;
+                        }
+                    }
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < k; j++) {
-                System.out.print(c[i][j] + " ");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[i][j] + " ");
             }
             System.out.println();
         }
 
     }
 }
+дз:сортировка столбцов
